0e939ab9fee2e9b599ae849d0b78546be040d4de
AkenEagle <akeneagle@gmail.com>
2021-11-28 22:30:24 +0000

    Succesfully created manager and looping add more employees question

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const Manager = require("./lib/Manager");
 const askQuestions = async () => {
   // Ask manager questions
   const managerAnswers = await inquirer.prompt(managerQuestions);
-  //   Create manager
-  const teamManager = new Manager(managerAnswers.manager_name);
+  //   Create manager from answers
+  const teamManager = new Manager(
+    managerAnswers.manager_name,
+    managerAnswers.manager_id,
+    managerAnswers.manager_email,
+    managerAnswers.manager_office
+  );
   // Ask add more employees
-  const moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
+  let moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
+  //   If the user didn't select finish
+  while (moreEmployeesAnswers.add_more_list !== "FINISH") {
+    console.log("SELECT FINISH!!!");
+    moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
+  }
 };
 
 askQuestions();
