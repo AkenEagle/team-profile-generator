68a7a2fd8e5bffacddd8fbb399a75d85d89571cb
AkenEagle <akeneagle@gmail.com>
2021-11-28 23:02:49 +0000

    Asked all questions succesfully and console logged the data

--- src/index.js
+++ src/index.js
@@ -1,24 +1,70 @@
 const inquirer = require("inquirer");
-const { managerQuestions, addMoreEmployees } = require("./lib/questions");
+const {
+  managerQuestions,
+  addMoreEmployees,
+  engineerQuestions,
+  internQuestions,
+} = require("./lib/questions");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-const askQuestions = async () => {
-  // Ask manager questions
-  const managerAnswers = await inquirer.prompt(managerQuestions);
-  //   Create manager from answers
-  const teamManager = new Manager(
+const createManager = (managerAnswers) => {
+  return new Manager(
     managerAnswers.manager_name,
     managerAnswers.manager_id,
     managerAnswers.manager_email,
     managerAnswers.manager_office
   );
-  // Ask add more employees
-  let moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
-  //   If the user didn't select finish
-  while (moreEmployeesAnswers.add_more_list !== "FINISH") {
-    console.log("SELECT FINISH!!!");
-    moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
-  }
 };
 
-askQuestions();
+const createEngineer = (engineerAnswers) => {
+  return new Engineer(
+    engineerAnswers.engineer_name,
+    engineerAnswers.engineer_id,
+    engineerAnswers.engineer_email,
+    engineerAnswers.engineer_github
+  );
+};
+
+const createIntern = (internAnswers) => {
+  return new Intern(
+    internAnswers.intern_name,
+    internAnswers.intern_id,
+    internAnswers.intern_email,
+    internAnswers.intern_school
+  );
+};
+
+const askTeamQuestions = async () => {
+  // Ask manager questions
+  const managerAnswers = await inquirer.prompt(managerQuestions);
+
+  //   Create manager from answers
+  const teamManager = createManager(managerAnswers);
+
+  // Ask add more employees
+  let moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
+
+  let engineers = [];
+  let interns = [];
+
+  //   If the user didn't select finish
+  while (moreEmployeesAnswers.add_more_list !== "FINISH") {
+    if (moreEmployeesAnswers.add_more_list === "Add ENGINEER") {
+      const engineerAnswers = await inquirer.prompt(engineerQuestions);
+      const teamEngineer = createEngineer(engineerAnswers);
+      engineers.push(teamEngineer);
+      moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
+    } else {
+      const internAnswers = await inquirer.prompt(internQuestions);
+      const teamIntern = createIntern(internAnswers);
+      interns.push(teamIntern);
+      moreEmployeesAnswers = await inquirer.prompt(addMoreEmployees);
+    }
+  }
+
+  console.log(teamManager, engineers, interns);
+};
+
+askTeamQuestions();
--- src/lib/questions.js
+++ src/lib/questions.js
@@ -1,22 +1,22 @@
 const managerQuestions = [
   {
     name: "manager_name",
-    message: "What's your team manager's name?",
+    message: "What's the team manager's name?",
     type: "input",
   },
   {
     name: "manager_id",
-    message: "What's your team manager's employee ID?",
+    message: "What's the team manager's employee ID?",
     type: "number",
   },
   {
     name: "manager_email",
-    message: "What's your team manager's email address?",
+    message: "What's the team manager's email address?",
     type: "input",
   },
   {
     name: "manager_office",
-    message: "What's your team manager's office number?",
+    message: "What's the team manager's office number?",
     type: "number",
   },
 ];
@@ -31,4 +31,55 @@ const addMoreEmployees = [
   },
 ];
 
-module.exports = { managerQuestions, addMoreEmployees };
+const engineerQuestions = [
+  {
+    name: "engineer_name",
+    message: "What's the engineer's name?",
+    type: "input",
+  },
+  {
+    name: "engineer_id",
+    message: "What's the engineer's employee ID?",
+    type: "number",
+  },
+  {
+    name: "engineer_email",
+    message: "What's the engineer's email address?",
+    type: "input",
+  },
+  {
+    name: "engineer_github",
+    message: "What's the engineer's GitHub username?",
+    type: "input",
+  },
+];
+
+const internQuestions = [
+  {
+    name: "intern_name",
+    message: "What's the intern's name?",
+    type: "input",
+  },
+  {
+    name: "intern_id",
+    message: "What's the intern's employee ID?",
+    type: "number",
+  },
+  {
+    name: "intern_email",
+    message: "What's the intern's email address?",
+    type: "input",
+  },
+  {
+    name: "intern_school",
+    message: "What's the intern's school?",
+    type: "input",
+  },
+];
+
+module.exports = {
+  managerQuestions,
+  addMoreEmployees,
+  engineerQuestions,
+  internQuestions,
+};
